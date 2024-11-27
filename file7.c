4d43bbc5800310df14b81fb6e5aca9bdc7ff11ea
Sajan Bisht <sajanb225@gmail.com>
2024-11-27 23:16:39 +0530

    corrected

--- file7.c
+++ file7.c
@@ -1,125 +1,129 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-typedef struct single_ll
-{
-    int no;
-    char str[50];
-    struct single_ll *next;
-}ll;
-ll* insert(ll*);
-ll* delete(ll*);
-void display(ll*);
-ll* del_smin(ll*);
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{
-    ll* top=NULL,*t=NULL;
+typedef struct single_ll {
+    int data;
+    struct single_ll* next;
+} ll;
+
+ll* insert(ll* top);
+ll* delete(ll* t);
+void display(ll* t);
+ll* del_smin(ll* t);
+
+int main() {
+    ll* top = NULL, * t = NULL;
     int n;
-    do
-    {
-        printf("enter 1-insert,2-delete,3-display,4-delete duplicate\n");
-        scanf("%d",&n);
-        switch(n)
-        {
-            case 1:
-            top=insert(top);
-            if( t==NULL)
-            {
-                    t=top;
-            }
-            if(t==NULL)
-            {
-                t=top;
+    do {
+        printf("Enter 1 - insert, 2 - delete, 3 - display, 4 - delete second minimum: ");
+        scanf("%d", &n);
+        switch (n) {
+        case 1:
+            top = insert(top);
+            if (t == NULL) {
+                t = top;
             }
             break;
-            case 2:
-            t=delete(t);
-            if(t==NULL)
-                top=NULL;
+        case 2:
+            t = delete(t);
+            if (t == NULL)
+                top = NULL;
             break;
-            case 3:
+        case 3:
             display(t);
             break;
-            case 4:
-            t=del_sim(t);
+        case 4:
+            top = t;
+            t = del_smin(t);
+            break;
         }
-    } while (n<=4);
+    } while (n <= 4);
     return 0;
 }
-ll* insert(ll*top)
-{
-    ll* p=NULL;
-    p=(ll*)malloc(sizeof(ll));
-    if(p!=NULL)
-    {
-        printf("enter name and roll no.\n");
-        scanf("%s",&p->str);
-        scanf("%d",&p->no);
-        if(top!=NULL)
-        {
-            top->next=p;
-            top=p;
-        }
 
+ll* insert(ll* top) {
+    ll* p = NULL;
+    p = (ll*)malloc(sizeof(ll));
+    if (p != NULL) {
+        printf("Enter data: ");
+        scanf("%d", &p->data);
+        p->next = NULL;
+
+        if (top == NULL) {
+            top = p;  // Insert at the beginning if the list is empty
+        } else {
+            ll* temp = top;
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+            temp->next = p;  // Insert at the end
+        }
+    } else {
+        printf("Memory allocation failed\n");
     }
-    else
-    printf("memory allocation failed\n");
-    p->next=NULL;
-    return p;
+    return top;
 }
-ll* delete(ll*t)
-{
-    if(t==NULL)
-    printf("Empty\n");
-    else
-    {
-    printf("deleted name and rollno\t:%s%d\t",t->str,t->no);
-    t=t->next;
+
+ll* delete(ll* t) {
+    if (t == NULL) {
+        printf("Empty list\n");
+    } else {
+        ll* temp = t;
+        printf("Deleted: %d\n", temp->data);
+        t = t->next;
+        free(temp);  // Free the memory of the deleted node
     }
     return t;
 }
-void display(ll*t)
-{
-    if(t==NULL)
-    printf("Empty\n");
-    while(t!=NULL)
-    {
-        printf("%s\n%d\n",t->str,t->no);
-        t=t->next;
+
+void display(ll* t) {
+    if (t == NULL) {
+        printf("Empty list\n");
+    } else {
+        while (t != NULL) {
+            printf("%d\n", t->data);
+            t = t->next;
+        }
     }
 }
-ll* del_smin(ll* t)
-{
-    ll* head=t,temp,min=NULL,min2,tt;
 
-    if(t!=NULL || t->next!=NULL)
-    {
-    	while(t!=NULL)
-    	{
-    		if(t->data <(t->next)->data)
-    		{ min2=min;
-    			min=t->data;
-    			}
-    		else if(t->data =(t->next)->data);
-    			
-    		else
-    		{min2=min;
-    			min=(t->next)->data;
-    			}
-    	}
-    	t=head;
-    	tt=t->next;
-    	while(t!=NULL)
-    	{
-    		if(min2==t->data)
-    		{
-    			
-    		}
-    	}
+ll* del_smin(ll* t) {
+    if (t == NULL || t->next == NULL) {
+        printf("List is too short to have a second minimum\n");
+        return t;  // Return the list as it is if there are less than 2 elements
     }
-    else
-    	printf("list is short\n");
-    	if(
+
+    ll *head = t, *temp = t, *prev = NULL, *minNode = NULL;
+    int min = __INT_MAX__, min2 = __INT_MAX__;
+
+    // First pass to find the minimum and second minimum
+    while (temp != NULL) {
+        if (temp->data < min) {
+            min2 = min;
+            min = temp->data;
+        } else if (temp->data < min2 && temp->data != min) {
+            min2 = temp->data;
+        }
+        temp = temp->next;
+    }
+
+    // Second pass to find and delete the second minimum node
+    temp = head;
+    prev = NULL;
+    while (temp != NULL) {
+        if (temp->data == min2) {
+            // If it's the first node
+            if (temp == head) {
+                head = temp->next;  // Re-link the head if the second minimum is the first node
+            } else {
+                prev->next = temp->next;  // Skip the second minimum node
+            }
+            free(temp);  // Free the memory of the deleted node
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+
     return head;
 }
